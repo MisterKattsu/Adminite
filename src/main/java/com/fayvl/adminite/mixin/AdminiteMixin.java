86e9de28d47696c8a530203d2ba0f687c83d7b06
Preston <eliteprime4199@gmail.com>
2025-01-14 13:20:45 -0500

    Overwrite repository with current code

---
+++ src/main/java/com/fayvl/adminite/mixin/AdminiteMixin.java
@@ -0,0 +1,34 @@
+package com.fayvl.adminite.mixin;
+
+import com.fayvl.adminite.Adminite;
+import com.fayvl.adminite.imgui.ImGuiImpl;
+import imgui.ImGui;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class AdminiteMixin {
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info) {
+        Adminite.LOGGER.info("This line is printed by an example mod mixin!");
+    }
+
+    @Inject(method = "render", at = @At("RETURN"))
+    private void render(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        ImGuiImpl.draw(io -> {
+            // Example on how to use a custom Font
+            // ImGui.pushFont(ImGuiImpl.defaultFont);
+            ImGui.begin("Hello World");
+			// Draw something here, see the official example module for more information:
+			// https://github.com/ocornut/imgui/blob/master/imgui_demo.cpp
+            ImGui.end();
+
+            ImGui.showDemoWindow();
+            // ImGui.popFont();
+        });
+    }
+}
